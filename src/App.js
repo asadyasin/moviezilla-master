c6ecfc8a07119250b1c8c8277a37d1c37e6b93e1
Asad Yasin <51986208+asadyasin@users.noreply.github.com>
2022-10-19 12:21:09 -0700

    Api change

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import MovieCard from './MovieCard';
 import './App.css';
 import SearchIcon from './search.svg';
 
-const API_URL = `http://www.omdbapi.com`;
-
+const API_URL2=`https://api.themoviedb.org/3/search/movie?api_key=`;
+const API_KEY = `4dfdddbcf3d766c55de7797df062b013`;
 const App = () => {
     
     const [movies , setMovies] = useState([]);
     const [searchTerm, setSearchTerm] =useState('');
     const searchMovies = async (title) =>{
-        const response = await fetch(`${API_URL}/?s=${title}&apikey=955702c3`);
+        const response = await fetch(`${API_URL2}${API_KEY}&query=${title}`);
         const data = await response.json();
-        setMovies(data.Search);
+        setMovies(data.results);
+        console.log(data.results);
     }
 useEffect(() => {
-    searchMovies('godzilla');
+    searchMovies('batman');
 }, []); 
   return (
         <div className='app'>
@@ -37,6 +38,7 @@ useEffect(() => {
                 <div className='container'>
                     {movies.map((movie,index)=>(
                         <MovieCard key={index} movie={movie}/>
+                        
                     ))
                     }
                 </div>) 
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-
+const image_url = "https://www.themoviedb.org/t/p/w220_and_h330_face/"
 const MovieCard =({movie})=>{
     return(
         <div className='movie'>
             <div>
-                <p>{movie.Year}</p>
+                <p>{movie.release_date}</p>
             </div>
             <div>
-                <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.Title}/>    
+                <img src={ movie.poster_path !== null ? `${image_url}${movie.poster_path}`:'https://via.placeholder.com/400?text=No+Image'} alt={movie.Title}/>    
             </div>
             <div>
-                <span>{movie.Type}</span>
-                <h3>{movie.Title}</h3>
+                <span>Ratings: {movie.vote_average}</span>
+                <h3>{movie.title}</h3>
             </div>    
         </div>   
     );
